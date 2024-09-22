952f8ccd33d1d010baf901b2988ac4e7a9432763
Ilya Belyak <belyachok567811@gmail.com>
2024-09-23 00:32:14 +0300

    Fix: Make changes to the second stage of the project - add stats get endpoint and versions of the dependencies to the parent pom

--- ewm-main-service-spec/src/main/java/ru/practicum/controller/MainServiceController.java
+++ ewm-main-service-spec/src/main/java/ru/practicum/controller/MainServiceController.java
@@ -2,11 +2,16 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.practicum.dto.EndpointHitDto;
+import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.service.TestService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +22,10 @@ public class MainServiceController {
     @PostMapping("/hit")
     public Mono<EndpointHitDto> createHitTest() {
         return testService.testEndpointHit();
+    }
+
+    @GetMapping("/stats")
+    public Mono<List<ViewStatsDto>> getHitTest() {
+        return testService.getHitStatsTest();
     }
 }
